50a4d510feadd62cd8d5f5f785578d61eee21bc3
Duyile Tolulope Patrick <princericky2000@yahoo.com>
2017-04-27 15:22:49 +0100

    Add dummy test to testrun travis

---
+++ tests/inverted-index-test.js
@@ -0,0 +1,14 @@
+// const index = require('../src/inverted-index.js');
+
+
+// const input = '';
+
+const expect = require('expect');
+
+describe('Inverted index test', () => {
+  describe('Case for reading books data', () => {
+    it('should return "invalid" if the file content is not a valid JSON array', () => {
+      expect(2).toEqual(2);
+    });
+  });
+});
