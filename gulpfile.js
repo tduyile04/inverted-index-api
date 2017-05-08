9cd0977ff9cfdb763a8cb9c297032a49896c6cbe
Duyile Tolulope Patrick <princericky2000@yahoo.com>
2017-05-08 07:55:06 +0100

    add: coverage and runtests taskrunner

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,20 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const clean = require('gulp-clean');
 const jasmine = require('gulp-jasmine');
+const injectModules = require('gulp-inject-modules');
 const istanbulReport = require('gulp-istanbul-report');
 const coverageFile = require('./coverage/coverage.json');
-// const istanbul = require('gulp-istanbul');
+const istanbul = require('gulp-istanbul');
 
 gulp.task('default', ['watch-src-converters', 'watch-test-converters']);
 
-gulp.task('src-converter', () => {
+gulp.task('src-converter', ['clean-src'], () => {
   return gulp.src('./src/**/*.js')
              .pipe(babel())
              .pipe(gulp.dest('./build/src'));
 });
 
-gulp.task('test-converter', () => {
+gulp.task('test-converter', ['clean-test'], () => {
   return gulp.src('./tests/inverted-index-test.js')
              .pipe(babel())
              .pipe(gulp.dest('./build/tests'));
@@ -38,21 +39,38 @@ gulp.task('clean', () => {
              .pipe(clean());
 });
 
+gulp.task('clean-test', () => {
+  return gulp.src('./build/tests')
+             .pipe(clean());
+});
+gulp.task('clean-src', () => {
+  return gulp.src('./build/src')
+             .pipe(clean());
+});
+
 gulp.task('run-tests', () => {
-  gulp.src('./build/tests/inverted-index-test.js')
+  gulp.src('./tests/inverted-index-test.js')
+      .pipe(babel())
+      .pipe(injectModules())
       .pipe(jasmine());
 });
 
-// gulp.task('serve-dev', serve({
-//   root: ['.'],
-//   port: 3000
-// }));
+// gulp.task('serve', () => {
+//   gulp.src('./build/src/routes/app.js')
+//       .pipe(node);
+// });
 
-gulp.task('coverage', ['run-tests'], () => {
-  gulp.src('./build/tests/inverted-index-test.js', { read: false })
-    .pipe(jasmine())
+gulp.task('coverage', (cb) => {
+  gulp.src('./build/src/inverted-index.js')
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
     .on('finish', () => {
-      gulp.src(coverageFile)
-        .pipe(istanbulReport());
+      gulp.src('./build/tests/inverted-index-test.js')
+      .pipe(babel())
+      .pipe(injectModules())
+      .pipe(jasmine())
+      .pipe(istanbul.writeReports())
+      .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }))
+      .on('end', cb);
     });
 });
